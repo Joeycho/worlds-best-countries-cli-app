9b1823a5290f3db9713dbe3aff84d6a35a55192c
Joey Cho <cjy12182000@naver.com>
2018-06-22 13:16:46 +0000

    rename the folder and reorganize the files

---
+++ lib/worlds_best_countries.rb
@@ -0,0 +1,7 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./worlds_best_countires/version"
+require_relative "./worlds_best_countires/country"
+require_relative "./worlds_best_countires/cli"
---
+++ lib/worlds_best_countries/version.rb
@@ -0,0 +1,3 @@
+module WorldsBestCountries
+  VERSION = "0.1.0"
+end
